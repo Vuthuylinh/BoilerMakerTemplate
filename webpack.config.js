bb40b5509a81b0f1cb762a5c1b10b0c4d8089fd3
Linh Vu <thuylinh2490@gmail.com>
2021-01-06 21:10:01 -0500

    webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const isDev = process.env.NODE_ENV === 'development'
+
+module.exports = {
+  mode: isDev ?'development':'production',
+  entry: [
+    '@babel/polyfill', // enables async-await
+    './client/index.js'
+  ],
+  output:{
+    path:__dirname,
+    filename:'./public/bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devtool:'source-map',
+  module:{
+    rules:[
+      {
+        test:/\.jsx?$/,
+        exclude:/node_modules/,
+        loader:'babel-loader'
+      },
+      // use the style-loader/css-loader combos for anything matching the .css extension
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  }
+
+
+}
